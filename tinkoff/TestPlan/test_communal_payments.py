5c6aae1405e4463cde9e70b0da2e02f4e419d9bb
oynovichkova <oynovichkova@yandex.ru>
2018-09-04 16:29:31 +0300

    delete

--- tinkoff/TestPlan/test_communal_payments.py
+++
@@ -1,7 +0,0 @@
-from Tests.PaymentsTest.communal_payments import *
-import pytest
-
-
-@pytest.allure.testcase('Оплата ЖКУ в Москве')
-def test_payment_gku_in_moscow(data):
-    payment_gku_in_moscow(data)
